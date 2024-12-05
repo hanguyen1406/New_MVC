9ea8b80e6b652fcb0fbfb7a685f3bab46386da0d
ha <hgnguyen47347714@gmail.com>
2024-12-05 08:33:47 +0700

    hi

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ require_once 'controllers/AdminController.php';
 $router = new Router();
 $router->add('/New_MVC', [new NewController(), 'index']);
 $router->dispatch($_SERVER['REQUEST_URI']);
-
+#thêm code ở đây
 ?>
